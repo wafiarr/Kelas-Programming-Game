c6b361bd1403ccb80115c8c30b9f991492122ebf
wafiarr <colemg80@gmail.com>
2021-08-13 15:16:18 +0700

    latihan1

---
+++ Latihan1/Assets/Scripts/latihan1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class latihan1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Selamat Siang");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
