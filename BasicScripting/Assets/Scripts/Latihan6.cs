681b02b3849943f73df0ac543955b84a99b038f2
wafiarr <colemg80@gmail.com>
2021-08-20 17:10:41 +0700

    Latihan 4,5,6

---
+++ BasicScripting/Assets/Scripts/Latihan6.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan6 : MonoBehaviour
+{
+    public int[] intArray; // Deklarasi Array
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Menampilkan Seluruh Array");
+        foreach (int a in intArray)
+        {
+            Debug.Log(a);
+        }
+        Debug.Log("Nilai Index ke 2 adalah " + intArray[2]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
